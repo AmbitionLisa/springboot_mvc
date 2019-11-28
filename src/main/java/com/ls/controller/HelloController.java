f75df8580b10869b2560f3c98fa929e7d7762861
chen <chen@qq.com>
2019-11-28 20:57:24 +0800

    hhh

--- src/main/java/com/ls/controller/HelloController.java
+++ src/main/java/com/ls/controller/HelloController.java
@@ -8,6 +8,7 @@ public class HelloController {
     @RequestMapping("hello")
 public  String hello(){
     System.out.println("我爱学zha1 ");
+        System.out.println("齐天大圣");
     return "hello springboot";
 }
 
